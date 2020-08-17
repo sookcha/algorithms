89bdc6d9f9dc901441ffdcde9b40474913e02001
Hoseong Son <lsookcha@me.com>
2020-08-17 09:42:54 +0000

    Add implementation for heapsort, mergesort

--- sort/heapsort.py
+++ sort/heapsort.py
@@ -1,23 +1,57 @@
 arr = []
 
+def insert(arr, data):
+    arr.append(data)
+    i = len(arr) - 1
 
-def sort():
+    if (len(arr) < 1):
+        return arr
+
+    parent = arr[(i-1)//2]
+    while (arr[(i-1)//2] < data and i != 0):
+        parent_data = parent
+        arr[(i-1)//2] = data
+        arr[i] = parent_data
+
+        i = (i-1)//2
+
+    return arr
+
+def sort(arr):
     """
-    부모 노드 가져오고 삭제 후
+    루트 노드 가져오고 삭제 후
     모든 노드가 텅텅 될 때까지 max_heapify 호출
     """
+    result = []
+    
+    while len(arr) != 0:
+        result = [arr.pop(0)] + result
+        if(len(arr) == 0):
+            break
+    
+        last_index = len(arr) - 1
+        arr = [arr.pop()] + arr
+        
+        max_heapify(arr, 0)
+
+    return result
+
 
 """
 부모는 (i-1)/2
 자식은 2i+1, 2i+2
 """
-def max_heapify(i):
+def max_heapify(arr, i):
     current = arr[i]
-    child_1 = arr[2i+1]
-    child_2 = arr[2i+2]
+    try:
+        child_1 = arr[(2*i)+1]
+    except:
+        child_1 = 0
 
-    if(not child_1 and not child_2):
-        return
+    try:
+        child_2 = arr[(2*i)+2]
+    except:
+        child_2 = 0
 
     if (child_1 < current and child_2 < current):
         return
@@ -26,4 +60,25 @@ def max_heapify(i):
     child_1과 child_2 사이에 더 큰값을 current와 바꾼 후,
     바뀐 current 자리에서 max_heapify 호출
     """
+    if (child_1 > child_2):
+        addr = (2*i)+1
+    else:
+        addr = (2*i)+2
 
+    temp = arr[i]
+    arr[i] = arr[addr]
+    arr[addr] = temp
+
+    i = addr
+    
+    max_heapify(arr, i)
+
+    return arr
+
+if __name__ == "__main__":
+    arr = insert(arr, 1)
+    arr = insert(arr, 3)
+    arr = insert(arr, 4)
+    arr = insert(arr, 2)
+    print(arr)
+    print(sort(arr))
--- sort/mergesort.py
+++ sort/mergesort.py
@@ -4,12 +4,49 @@
 한 쪽만 완료됐으면 남은 부분은 그대로 옮김
 tmp -> original array로 옮김
 """
-def merge():
-    pass
+def merge(arr, arr_1, arr_2):
+    tmp = []
+    i = 0
+    j = 0
+    k = 0
+
+    while(i < len(arr_1) and j < len(arr_2)):
+        if (arr_1[i] > arr_2[j]):
+            tmp.append(arr_2[j])
+            j+=1
+        else:
+            tmp.append(arr_1[i])
+            i+=1
+
+    while(i < len(arr_1)):
+        tmp.append(arr_1[i])
+        i+=1
+
+    while(j < len(arr_2)):
+        tmp.append(arr_2[j])
+        j+=1
+
+    for index in range(len(tmp)):
+        arr[index] = tmp[index]
+
+    return arr
 
 """
 주어진 array를 반띵하고 merge 하는 것 반복
 array length가 1이 될 때까지
 """
-def divide():
-    pass
+def divide(arr):
+    if len(arr) <= 1:
+        return
+
+    half = len(arr) // 2
+    arr_1 = arr[:(half)]
+    arr_2 = arr[half:len(arr)]
+
+    divide(arr_1)
+    divide(arr_2)
+
+    return merge(arr, arr_1, arr_2)
+
+if __name__ == '__main__':
+    print(divide([5,3,6,1,2]))
